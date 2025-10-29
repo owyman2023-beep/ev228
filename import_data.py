2e1abca7a8a088d355b4bcec0c9d880d7f56aa79
owyman2023-beep <o_wyman2023@coloradocollege.edu>
2025-10-29 09:22:03 -0600

    Update import_data.py

--- import_data.py
+++ import_data.py
@@ -9,11 +9,15 @@ df_cos = pd.read_csv(fn_data)
 
 print(df_cos)
 
+max_temp = df_cos['maximumT']
+dates = df_cos['Year_Month']
 
+max_temp = max_temp[1:]
+dates = dates[1:]
 
 fig, ax = plt.subplots(figsize=(12,6))
 ax.plot(dates, max_temp)
 ax.set_xlabel('Year')
 ax.set_ylabel('Maximum Temperature (°F)')
-ax.set_title('Monthly Maximum Temperature in Colorado Springs')
+ax.set_title('Monthly Maximum Temperature at KRDU')
 plt.show()
