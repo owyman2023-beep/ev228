40738f5a8512017dd6f9d71ac2f7ee22f0d42464
owyman2023-beep <o_wyman2023@coloradocollege.edu>
2025-10-29 09:06:23 -0600

    Update import_data.py

--- import_data.py
+++ import_data.py
@@ -6,3 +6,14 @@ fn_data = '/content/KRDU_temp_188708-202508.csv'
 
 
 df_cos = pd.read_csv(fn_data)
+
+print(df_cos)
+
+
+
+fig, ax = plt.subplots(figsize=(12,6))
+ax.plot(dates, max_temp)
+ax.set_xlabel('Year')
+ax.set_ylabel('Maximum Temperature (°F)')
+ax.set_title('Monthly Maximum Temperature in Colorado Springs')
+plt.show()
