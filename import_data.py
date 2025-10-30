9540e4cd4dd3cfe983be7590310ea95b35a02295
owyman2023-beep <o_wyman2023@coloradocollege.edu>
2025-10-30 09:16:45 -0600

    Update import_data.py

--- import_data.py
+++ import_data.py
@@ -1,16 +1,19 @@
+
 import pandas as pd
+import matplotlib.pyplot as plt
+
+path_data = '/Users/owenwyman/Data/ev228_data/'
+fn_data = 'KRDU_temp_188708-202508.csv'
 
 
-path_data = '/User/[owenwyman]/Data/ev228_data/'
-fn_data = '/content/KRDU_temp_188708-202508.csv'
-
-
-df_cos = pd.read_csv(fn_data)
+df_cos = pd.read_csv(path_data + fn_data)
 
 print(df_cos)
 
-max_temp = df_cos['maximumT']
-dates = df_cos['Year_Month']
+max_temp = df_cos['metANN']
+
+print(max_temp)
+dates = df_cos['YEAR']
 
 max_temp = max_temp[1:]
 dates = dates[1:]
@@ -18,6 +21,6 @@ dates = dates[1:]
 fig, ax = plt.subplots(figsize=(12,6))
 ax.plot(dates, max_temp)
 ax.set_xlabel('Year')
-ax.set_ylabel('Maximum Temperature (°F)')
-ax.set_title('Monthly Maximum Temperature at KRDU')
+ax.set_ylabel('Annual Mean Temperature (°C)')
+ax.set_title('Annual Mean Temperature')
 plt.show()
