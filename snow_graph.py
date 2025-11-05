775bac1319d6fba4fd5c90a19fa91479bd117c35
owyman2023-beep <o_wyman2023@coloradocollege.edu>
2025-11-04 20:27:40 -0700

    Update snow_graph.py

--- snow_graph.py
+++ snow_graph.py
@@ -10,17 +10,18 @@ fn = 'faf2d27731ed6827c0cb7c284be3f43b.nc'
 out_path = '/Users/owenwyman/Data/ev228_data/figures/'
 out_fn = '1_proj.png'
 
-da = fi.import_era5(file_path=path + fn, var='si10')
+
+
+da = fi.import_era5(file_path=path + fn, var='csf')
 da_timemn = da.mean(dim='valid_time')
-da_toplot = da_timemn - 273.15
-fp.map(da_toplot, out_path=out_path, out_name=out_fn)
+# da_toplot = da_timemn 
+fp.map(da_timemn, out_path=out_path, out_name=out_fn)
 
 
 
-# mean_var = np.mean(filter_data)
-# stdev_var = np.std(filter_data)
-# max_var = np.max(filter_data)
-# min_var = np.min(filter_data)
+# mean_var = np.mean(da_timemn)
+# stdev_var = np.std(da_timemn)
+# max_var = np.max(da_timemn)
+# min_var = np.min(da_timemn)
 # print(mean_var, stdev_var, max_var, min_var)
 
-# fp.timeseries(filter_data, in_x=filter_year, out_path=fig_path, out_name=fig_name)
