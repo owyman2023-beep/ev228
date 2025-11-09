ba68da2f797cbfee2947e9373bd40f7a1a798b5c
owyman2023-beep <o_wyman2023@coloradocollege.edu>
2025-11-09 16:59:49 -0700

    Update snow_graph.py

--- snow_graph.py
+++ snow_graph.py
@@ -5,29 +5,44 @@ import pandas as pd
 import numpy as np
 import xarray as xr
 import cartopy.crs as ccrs
+import cartopy.feature as cf
 
 path = '/Users/owenwyman/Data/ev228_data/individual_project/'
-fn = 'faf2d27731ed6827c0cb7c284be3f43b.nc'
+fn = 'edd98c954b619fc3f8c3ad46add9b76c.nc'
 out_path = '/Users/owenwyman/Data/ev228_data/figures/'
-out_fn = '1_proj.png'
+out_fn = '4_proj.png'
 
 
-da = fi.import_era5(file_path=path + fn, var='csf')
-da_timemn = da.mean(dim='valid_time')
-# da_toplot = da_timemn 
-fp.map(da_timemn, out_path=out_path, out_name=out_fn)
-
-ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-ax.coastlines(resolution='10m')
-ax.add_feature(cartopy.feature.STATES, linestyle=':')
-ax.add_feature(cartopy.feature.BORDERS, linestyle='--')
+da = fi.import_era5(file_path=path + fn, var='sde')
 
 
 
+mu = da.mean(dim = 'valid_time')
+sigma = da.std(dim = 'valid_time')
+z_score = (da-mu) / sigma
+da1 = z_score.sel(valid_time = '2024-02-01')
 
-# mean_var = np.mean(da_timemn)
-# stdev_var = np.std(da_timemn)
-# max_var = np.max(da_timemn)
-# min_var = np.min(da_timemn)
-# print(mean_var, stdev_var, max_var, min_var)
+# da_timemn = da.mean(dim='valid_time')
+
+projection = ccrs.PlateCarree()
+fig = plt.figure(figsize=(10, 8))
+ax = plt.axes(projection = ccrs.PlateCarree())
+da1.plot.pcolormesh(
+    x = 'longitude',
+    y = 'latitude',
+    ax = ax,
+    transform = ccrs.PlateCarree(),
+    extend = 'both'
+)
+ 
+ax.add_feature(cf.STATES, linestyle=':')
+ax.add_feature(cf.BORDERS, linestyle='--')
+
+ax.set_extent([-109.3, -101.8, 36.8, 41.2], crs=ccrs.PlateCarree())
+ax.set_title('Cumulative Snowfall in Colorado (1950-2025)')
+
+plt.savefig(out_path + out_fn, dpi=300, bbox_inches='tight') 
+plt.show()
+
+
 
