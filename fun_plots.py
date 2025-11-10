8fdcf990c24da971dfaa22f43aad2fba6d5a1b01
owyman2023-beep <o_wyman2023@coloradocollege.edu>
2025-11-09 17:00:37 -0700

    Update fun_plots.py

--- fun_plots.py
+++ fun_plots.py
@@ -2,6 +2,8 @@ import sys
 from icecream import ic
 import matplotlib.pyplot as plt
 import numpy as np
+import cartopy.crs as ccrs
+import cartopy.feature as cf
 
 def timeseries(in_df, in_x=None, out_path='', out_name=''):
     ''' Plot timeseries from 1D dataframe '''
@@ -21,8 +23,8 @@ def map(in_da, out_path='', out_name=''):
     ax = fig.add_subplot(111)
     lons = in_da.longitude
     lats = in_da.latitude
-
     image = plt.pcolormesh(lons, lats, in_da)
+
     plt.xlabel('longitude')
     plt.ylabel('latitude')
     plt.title('Cumulative Snowfall in Colorado from 1940-2024')
@@ -30,14 +32,14 @@ def map(in_da, out_path='', out_name=''):
     cb.set_label('in')
     plt.savefig(out_path + out_name, dpi=400)
 
-def bar(in_df, in_x=None, out_path='', out_name=''):
+# def bar(in_df, in_x=None, out_path='', out_name=''):
 
-    fig, ax = plt.subplots(figsize=(8,6))
-    ax.bar(annual_mean.index, annual_mean.values, color='skyblue', edgecolor='darkblue', linewidth=2, width=0.8, label=stats_label)
-    ax.set_ylim(bottom=5, top=8)
-    ax.set_xlabel('Year')
-    ax.set_ylabel('Average Halibut Weight (kg)')
-    ax.set_title('Pacific Halibut Weight from 2019-2024')
-    ax.set_xticks(annual_mean.index)
-    ax.legend(loc='best', title="Statistics (2019-2025)")
-    plt.savefig(out_path + out_name, dpi=400)
+#     fig, ax = plt.subplots(figsize=(8,6))
+#     ax.bar(annual_mean.index, annual_mean.values, color='skyblue', edgecolor='darkblue', linewidth=2, width=0.8, label=stats_label)
+#     ax.set_ylim(bottom=5, top=8)
+#     ax.set_xlabel('Year')
+#     ax.set_ylabel('Average Halibut Weight (kg)')
+#     ax.set_title('Pacific Halibut Weight from 2019-2024')
+#     ax.set_xticks(annual_mean.index)
+#     ax.legend(loc='best', title="Statistics (2019-2025)")
+#     plt.savefig(out_path + out_name, dpi=400)
