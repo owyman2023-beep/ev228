01985c3c59e41ceb8936fe1beb112acb716b93ca
owyman2023-beep <o_wyman2023@coloradocollege.edu>
2025-11-09 17:00:26 -0700

    Update practical6_individual_satation_data.py

--- practical6_individual_satation_data.py
+++ practical6_individual_satation_data.py
@@ -3,6 +3,7 @@ import fun_import as fi
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 file_path = "/Users/owenwyman/Data/ev228_data/practical6/"
 file_name = 'SGM00061600_temp_189201-202508.csv'
@@ -15,9 +16,6 @@ time_var = 'YEAR'
 df, df_year = fi.import_ghcn(file_path=file_path + file_name, var=var)
 filter_data = df[df != 999.9]
 filter_year = df_year[df != 999.9]
-
-# print(filter_data)
-# print(df_year)
 
 mean_var = np.mean(filter_data)
 stdev_var = np.std(filter_data)
